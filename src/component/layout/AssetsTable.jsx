a4d08e43887c88c0cd3e04d4b261a98ebb5113bb
unknown <168nwirk@gmail.com>
2025-01-20 17:40:17 +0800

    add table assets

--- src/component/layout/AssetsTable.jsx
+++ src/component/layout/AssetsTable.jsx
@@ -1,3 +1,40 @@
-export default function AssetsTable() {
-    return <div>111</div>
-}
+import React from 'react';
+import { Table } from 'antd';
+import { useCrypto } from '../contex/CryptoContext';
+const columns = [
+    {
+      title: 'Name',
+      dataIndex: 'name',
+      sorter: (a, b) => a.name.length - b.name.length,
+      width: '30%',
+    },
+    {
+      title: 'Price, $',
+      dataIndex: 'price',
+      defaultSortOrder: 'descend',
+      sorter: (a, b) => a.price - b.price,
+    },
+    {
+      title: 'Amount',
+      dataIndex: 'amount',
+      defaultSortOrder: 'descend',
+      sorter: (a, b) => a.amount - b.amount,
+    }
+  ];
+
+
+  export default function AssetsTable() {
+    const {assets} = useCrypto()
+
+    const data = assets.map((a) => ({
+        key: a.id,
+        name: a.name,
+        price: a.price,
+        amount: a.amount,
+    }))
+            return (<Table 
+                pagination={false} 
+                columns={columns} 
+                dataSource={data} />)
+  }
+  
